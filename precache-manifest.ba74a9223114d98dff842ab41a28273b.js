59aaca4e84dc730a7b75679fc185fd015e0b081c
chernikovmax <chernik.max@gmail.com>
2019-04-05 16:52:05 +0400

    Updates

---
+++ precache-manifest.ba74a9223114d98dff842ab41a28273b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00bb85d0cdff961cbb83",
+    "url": "/react-todo-page/static/css/main.717873eb.chunk.css"
+  },
+  {
+    "revision": "00bb85d0cdff961cbb83",
+    "url": "/react-todo-page/static/js/main.b11570d6.chunk.js"
+  },
+  {
+    "revision": "c5e04804231cfcbe349a",
+    "url": "/react-todo-page/static/js/runtime~main.0c5fe1a3.js"
+  },
+  {
+    "revision": "7b9f41c871b61d3ee0db",
+    "url": "/react-todo-page/static/js/2.0099f0e5.chunk.js"
+  },
+  {
+    "revision": "04b1a253487edfe729c63c6525130e3f",
+    "url": "/react-todo-page/index.html"
+  }
+];
